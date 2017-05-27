0db2a48940442b7dfaac8bf5641544996e433195
César Rodríguez <cesar.rodriguez.lara54@gmail.com>
2017-05-27 14:58:54 -0500

    Se han creado los movimientos básicos del pérsonaje y las colisiones con el suelo. Falta crear las colisiones con las plataformas

--- js/main.js
+++ js/main.js
@@ -11,7 +11,42 @@
     alto: 64,
     velocidad: 3,
     velY: 0,
-    salto: false
+    salto: false,
+    colisionIzquierdaSuelo: false,
+    colisionDerechaSuelo: false,
+    colisionInferiorSuelo: false,
+    colisionSuperiorSuelo: false,
+  };
+
+  var rectangulosJugador =
+  {
+    rectanguloSuperiorJugador: {
+      x: jugador.x,
+      y: jugador.y,
+      ancho: jugador.ancho,
+      alto: 5
+    },
+
+    rectanguloInferiorJugador: {
+      x: jugador.x,
+      y: jugador.y + jugador.alto,
+      ancho: jugador.ancho,
+      alto: 5
+    },
+
+    rectanguloIzquierdoJugador: {
+      x: jugador.x,
+      y: jugador.y,
+      ancho: 5,
+      alto: jugador.alto
+    },
+
+    rectanguloDerechoJugador: {
+      x: jugador.x + jugador.ancho,
+      y: jugador.y,
+      ancho: 5,
+      alto: jugador.alto
+    }
   };
 
   var suelo =
@@ -22,11 +57,23 @@
     alto: 30
   };
 
+  var rectangulosSuelo =
+  {
+    rectanguloSuperiorSuelo: {
+      x: suelo.x,
+      y: suelo.y,
+      ancho: suelo.ancho,
+      alto: 5
+    }
+  };
+
   var plataformas = [];
 
   var fondo;
 
   var teclado = [];
+  var friction = 0.8;
+  var gravity = 0.3;
 
   function crearPlataformas()
   {
@@ -101,28 +148,90 @@
   }
 
   function agregarEventosTeclado()
+	{
+		agregarEventos(document, "keydown", function(e)
+		{
+			teclado[e.keyCode] = true;
+		});
+
+		agregarEventos(document, "keyup", function(e)
+		{
+			teclado[e.keyCode] = false;
+		});
+
+		function agregarEventos(elemento, nombreEvento, funcion)
+		{
+			if(elemento.addEventListener)
+			{
+				elemento.addEventListener(nombreEvento,  funcion, false);
+			}
+			else if(elemento.attachEvent)
+			{
+				elemento.attachEvent(nombreEvento, funcion);
+			}
+		}
+	}
+
+  function actualizarRectangulosJugador()
   {
-    agregarEventos(document, "keydown", function(e)
-    {
-      teclado[e.keyCode] = true;
-    });
+    rectangulosJugador.rectanguloSuperiorJugador.x = jugador.x;
+    rectangulosJugador.rectanguloSuperiorJugador.y = jugador.y;
+    rectangulosJugador.rectanguloInferiorJugador.x = jugador.x;
+    rectangulosJugador.rectanguloInferiorJugador.y = jugador.y + jugador.alto;
+    rectangulosJugador.rectanguloIzquierdoJugador.x = jugador.x;
+    rectangulosJugador.rectanguloIzquierdoJugador.y = jugador.y;
+    rectangulosJugador.rectanguloDerechoJugador.x = jugador.x + jugador.ancho;
+    rectangulosJugador.rectanguloDerechoJugador.y = jugador.y;
+  }
 
-    agregarEventos(document, "keyup", function(e)
-    {
-      teclado[e.keyCode] = false;
-    });
+  function colisionInferiorJugadorSuelo()
+  {
+    if(rectangulosJugador.rectanguloInferiorJugador.y + rectangulosJugador.rectanguloInferiorJugador.alto >= rectangulosSuelo.rectanguloSuperiorSuelo.y)
+      jugador.colisionInferiorSuelo = true;
+    else
+      jugador.colisionInferiorSuelo = false;
+  }
 
-    function agregarEventos(elemento, nombreEvento, funcion)
+  function colisionesJugadorSuelo()
+  {
+    colisionInferiorJugadorSuelo();
+  }
+
+  function moverPersonaje()
+  {
+    colisionesJugadorSuelo();
+
+    if(jugador.colisionInferiorSuelo && teclado[38])
     {
-      if(elemento.addEventListener)
-      {
-        elemento.addEventListener(nombreEvento, funcion, false);
-      }
-      else if(elemento.attachEvent)
-      {
-        elemento.attachEvent(nombreEvento, funcion);
-      }
+      jugador.velY = -jugador.velocidad * 4;
+      jugador.colisionInferiorSuelo = false;
     }
+
+    if(teclado[39])
+      jugador.x += jugador.velocidad;
+
+    if(teclado[37])
+      jugador.x -= jugador.velocidad;
+
+    if(jugador.x + jugador.ancho < 0)
+      jugador.x = canvas.width;
+
+    if(jugador.x > canvas.width)
+      jugador.x = -64;
+
+    if(!jugador.colisionInferiorSuelo)
+    {
+      gravity = 0.3;
+      jugador.velY += gravity;
+    }
+
+    if(jugador.colisionInferiorSuelo)
+    {
+      gravity = 0;
+      jugador.velY = gravity;
+    }
+
+    jugador.y += jugador.velY;
   }
 
   function dibujarFondo()
@@ -162,10 +271,14 @@
 
   function main()
   {
+    actualizarRectangulosJugador();
+    moverPersonaje();
     dibujarFondo();
     dibujarSuelo();
     dibujarPlataformas();
     dibujarJugador();
+
+    console.log(jugador.colisionInferiorSuelo);
   }
 
   agregarEventosTeclado();
